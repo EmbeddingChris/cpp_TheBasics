d5970dd03006a25519240c1607339f38c7f3c27d
Christian Ebenau <c.ebenau@gmx.de>
2019-04-28 17:38:51 +0200

    added source code example of eps1.2_AnatomyOfCpp

---
+++ eps1-2_anatomyOfCpp.cpp
@@ -0,0 +1,30 @@
+/*
+ *  file: eps1-2_anatomyOfCpp.cpp
+ * 
+ *  GNU General Public License v3.0 
+ * 
+ *  author: embedding.chris
+ *  e-mail: embedding.chris@gmail.com
+ * 
+ *  date: 27.04.2019
+ *  last change: 27.04.2019
+ *
+ */
+
+// Preprocessor Directive
+#include <iostream>
+
+// The Body - Start of the Program
+
+// namespace
+using namespace std; 
+
+// main() Function
+int main (int argc, char* argv[])
+{    
+     // Output
+     cout << "Hello World!" << endl;
+
+     // Return Value
+     return 0;
+}
